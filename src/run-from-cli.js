9fe8a6b772b5380bebb7f612f54115b8a6c16424
Mathias Frey <mathias.c.frey@gmail.com>
2023-05-11 22:11:48 +0200

    JSON to tree parser working.

--- src/run-from-cli.js
+++ src/run-from-cli.js
@@ -2,6 +2,8 @@
 // const generate_package_name = require("./helpers");
 // const CSV2XML = require("./csv-2-xml.js");
 
+RESULT = []
+
 const fs = require('fs');
 
 console.log("Run fron CLI");
@@ -16,12 +18,13 @@ const data = JSON.parse(
 console.log('Byte length:', fs.readFileSync(jsonFile).byteLength);
 console.log(traverse(data));
 
+console.log(JSON.stringify(RESULT));
 
 
-function traverse(obj) {
+function traverse(obj, depth = 0, result=RESULT) {
 
     // TODO: a simple array input does not work [1,2,3]
-    // console.log(typeof obj);
+    // console.log('>>>', obj, typeof obj);
     // console.log(Array.isArray(obj));
 
     // iterate over all keys
@@ -30,19 +33,37 @@ function traverse(obj) {
       if (obj.hasOwnProperty(key)) {
         let value = obj[key];
         let type = typeof value;
+
+        console.log(`${' '.repeat(depth * 2) } ${key}: ${type}`);
         
+        // result["name"] = key; 
+        // result["type"] = type;
+
         if (Array.isArray(value)) {
           // process array
-          console.log(111);
-          processArray(value);
+          
+          
+          processArray(value, depth);
+          // do not traverse arrays of str or int
+          if ((typeof value[0] === 'object') || Array.isArray(value[0])) {
+            var children = [];
+            result.push({"name": key, "type": "Array", length: value.length, "children": children})
+            traverse(value[0], depth + 1, children);
+          } else {
+            // this is an array of num or str
+            result.push({"name": key, "type": `Array of ${typeof value[0]}`, length: value.length})
+          }
 
         } else if (type === 'object') {
           // process nested object
-          traverse(value);
+          var children = [];
+          result.push({"name": key, "type": type, "children": children})
+          traverse(value, depth + 1, children);
         } else if (type === 'string') {
-          console.log(222);
+          result.push({"name": key, "type": type})
+          //console.log(key, value, 'STR');
         } else if (type == 'number') {
-            console.log(key, value, 'NUM');
+          result.push({"name": key, "type": type})
         } else {
           // process other data types
           console.log('ELSE');
@@ -52,10 +73,11 @@ function traverse(obj) {
   }
 
 
-  function processArray(arr) {
+  function processArray(arr, depth) {
     if (isHomogeneous(arr)) {
-        console.log(`A homogeneous array of type ${typeof arr[0]}`)
+        console.log(`${' '.repeat(depth * 2)}A homogeneous array of type ${typeof arr[0]}`)
     }
+    console.log(`${' '.repeat(depth * 2)}Array of length ${arr.length}`)
   }
 
   function isHomogeneous(arr) {
